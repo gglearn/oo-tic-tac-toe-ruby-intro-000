18ec5354485d7945172f995a737006f5da997fcb
GG Learn <ozolsdeko@gmail.com>
2017-11-04 13:28:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,10 +54,10 @@ class TicTacToe
     input = gets.strip
     index = input_to_index(input)
     if valid_move?(index)
-      move(index, current_player(board))
+      move(index, current_player)
       display_board
     else
-      turn(board)
+      turn
     end
   end
 
